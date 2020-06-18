3f19462bd569ec1db0d6e36bc34ffe44e966e134
webobite <singhsubham510@gmail.com>
2020-06-18 18:20:28 +0530

    Added Country component and nav bar with route link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { CountryComponent } from './country/country.component';
+
+const appRoutes: Routes = [
+{
+    path: '',
+    component: HomeComponent
+},  
+{
+    path: 'country',
+    component: CountryComponent
+}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent, CountryComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
